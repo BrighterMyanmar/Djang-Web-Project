54c73952380905e13e197ba6c975e86a8b7f73c9
azphyo <azphyo72@gmail.com>
2021-01-17 18:51:05 +0630

    updated 2

--- category/urls.py
+++ category/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('',cats,name="all-cats"),
     path('subs',subs,name="all-sub-cats"),
-    path('subs/create',create,name="sub-create")
+    path('subs/create',create,name="sub-create"),
+    path('subs/<int:id>/delete',delete,name="sub-delete"),
+    path('subs/<int:id>/edit',edit,name="sub-edit")
 ]
--- category/views.py
+++ category/views.py
@@ -13,16 +13,33 @@ def subs(request):
     cats = SubCategory.objects.all();
     return render(request,'category/subs.html',{'cats':cats})
 
-
 def create(request):
     if request.method == "POST":
        form = SubCatFrom(request.POST,request.FILES)
        if form.is_valid():
            form.save() 
            return redirect('/cats/subs')
-        
+
     else :
         print("Get Method")
         form = SubCatFrom()
 
     return render(request,'category/create.html',{'form':form})
+
+def delete(request,id):
+    subcat = SubCategory.objects.get(pk=id)
+    subcat.delete()
+    return redirect('/cats/subs')
+
+def edit(request,id):
+    category = SubCategory.objects.get(pk=id)
+
+    if request.method == "POST":
+        form = SubCatFrom(request.POST,request.FILES,instance=category)
+        if form.is_valid():
+            form.save() 
+            return redirect('/cats/subs')
+    else :
+        form = SubCatFrom(instance=category)
+    
+    return render(request,'category/edit.html',{'form':form})
